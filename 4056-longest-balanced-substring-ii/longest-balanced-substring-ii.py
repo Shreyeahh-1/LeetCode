bcaa2c239246aebfb9137672f978140f09f3a957
Shreyeahh-1 <shreyamandal885@gmail.com>
2026-02-13 23:12:36 +0530

    Time: 4204 ms (5.11%) | Memory: 45.5 MB (62.77%) - LeetSync

---
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        res = 0
+        for mask in range(1, 8):
+            idx_map = {tuple([0] * (bin(mask).count('1') - 1)): -1}
+            counts = [0, 0, 0]
+
+            start_index = 0
+            for i, char in enumerate(s):
+                val = ord(char) - ord('a')
+
+                if not ((mask >> val) & 1):
+                    counts = [0, 0, 0]
+
+                    idx_map = {tuple([0] * (bin(mask).count('1') - 1)): i}
+                    start_index = i+1
+                    continue
+
+                counts[val] += 1
+
+                state = []
+                active_counts = [counts[j] for j in range(3) if (mask >> j) & 1]
+
+                if active_counts:
+                    base = active_counts[0]
+                    for k in range(1, len(active_counts)):
+                        state.append(active_counts[k] - base)
+
+                current_state = tuple(state)
+
+                if current_state in idx_map:
+                    res = max(res, i - idx_map[current_state])
+                else:
+                    idx_map[current_state] = i
+
+        return res
+
+        
